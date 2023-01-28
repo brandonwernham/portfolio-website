6630558957c60e7aae6cf446267cbe5ebe9ade7e
brandonwernham <brandonwern01@gmail.com>
2023-01-28 10:41:13 -0500

    Create all components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Education from './components/education/Education'
+import Projects from './components/projects/Projects'
+import Hobbies from './components/hobbies/Hobbies'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Education />
+      <Projects />
+      <Hobbies />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
