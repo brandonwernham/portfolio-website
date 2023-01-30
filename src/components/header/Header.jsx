f2ed367719e5f5912ea419dd3a7eabea879ebf8c
brandonwernham <brandonwern01@gmail.com>
2023-01-30 16:26:57 -0500

    Tiny css changes, pic change

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/IMG_1066.jpg'
+import ME from '../../assets/Me.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
+import {BsGithub} from 'react-icons/bs'
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/brandon-wernham-90771722a/" target="_blank"><BsLinkedin /></a>
-        <a href="https://github.com/brandonwernham" target="_blank"></a>
+        <a href="https://github.com/brandonwernham" target="_blank"><BsGithub /></a>
     </div>
   )
 }
