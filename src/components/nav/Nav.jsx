a1364392bbfdba3117bc4e000cd9ac6c716ebe4f
brandonwernham <brandonwern01@gmail.com>
2023-02-02 19:03:11 -0500

    Work on nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './nav.css'
-import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineHome, AiOutlineUser, AiOutlineBook, AiOutlineProject, AiOutlineHeart, AiOutlineMessage} from 'react-icons/ai'
 
 const Nav = () => {
   return (
     <nav>
       <a href="#"><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineUser /></a>
+      <a href="#education"><AiOutlineBook /></a>
+      <a href="#projects"><AiOutlineProject /></a>
+      <a href="#hobbies"><AiOutlineHeart /></a>
+      <a href="#contact"><AiOutlineMessage /></a>
     </nav>
   )
 }
