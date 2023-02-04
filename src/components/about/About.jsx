c6df340fdc08ffa629e91ca8114ef18b801d8883
brandonwernham <brandonwern01@gmail.com>
2023-02-04 18:48:05 -0500

    Work on about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './about.css'
 import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {MdOutlineWeb} from 'react-icons/md'
+import {AiFillDatabase} from 'react-icons/ai'
 
 const About = () => {
   return (
@@ -17,8 +20,41 @@ const About = () => {
 
         <div className="about__content">
           <div className="about__cards">
-            <article className='about__card'></article>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Coding Experience</h5>
+              <small>4+ Years Project-Based</small>
+            </article>
+
+            <article className='about__card'>
+              <MdOutlineWeb className='about__icon'/>
+              <h5>Frontend</h5>
+              <small>JavaScript</small><br></br>
+              <small>React</small><br></br>
+              <small>HTML5</small><br></br>
+              <small>CSS3</small>
+            </article>
+
+            <article className='about__card'>
+              <AiFillDatabase className='about__icon'/>
+              <h5>Backend</h5>
+              <small>Java</small><br></br>
+              <small>SQL</small><br></br>
+              <small>NodeJS</small><br></br>
+              <small>Python</small>
+            </article>
           </div>
+
+          <p>
+          I am a driven Software Engineer with 4+ years of programming experience. 
+          I help companies bring innovative ideas to life will full-stack development. 
+          I work well in a team, proven with group experience opportunities throughout 
+          my 4 years at Western University. I am customer-driven, always striving 
+          to make clients 100% satisfied. I have excellent experience with 
+          Java, JavaScript, React, HTML, CSS, Unity, and Node.js.
+          </p>
+
+          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
         </div>
       </div>
     </section>
