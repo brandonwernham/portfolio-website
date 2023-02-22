dda52742856f5b69d714d92296e5fa9fdba189a8
brandonwernham <brandonwern01@gmail.com>
2023-02-22 08:58:41 -0500

    Finish footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import './footer.css'
+import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Brandon Wernham</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/brandon-wernham-90771722a/"><AiFillLinkedin /></a>
+        <a href="https://github.com/brandonwernham"><AiFillGithub /></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Brandon Wernham. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
